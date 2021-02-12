a486b7f4d7432cf9795bc09cfddd707d5051d640
bluesweetpotato <gi020477@gmail.com>
2021-02-13 01:39:06 +0900

    동전1

--- coin1.py
+++ coin1.py
@@ -1,13 +1,18 @@
-n, m = map(int,input().split())
+# 백준 2293번 동전 1
+
+N, K = map(int,input().split())
 
 # 동전의 종류
-kind = []
-for _ in range(n):
-    kind.append(int(input()))
+kinds = []
+for _ in range(N):
+    kinds.append(int(input()))
 
-dp = [0]*100000
+dp = [0]*(K+1)
+dp[0] = 1
 
-for i in range(2,m+1):
-    for j in range(0,n):
-        if i > kind[j]:
-            dp[i] = dp[i-kind[j]] + 1
+for kind in kinds:
+    for i in range(K+1):
+        # 만약 동전의 값이 K원 보다 작거나 같으면
+        if i >= kind:
+            dp[i] += dp[i-kind]
+print(dp[K])
