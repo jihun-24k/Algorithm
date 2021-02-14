e80112183369b610a0a71e7bd6c495cb93c0021a
bluesweetpotato <gi020477@gmail.com>
2021-02-14 19:49:20 +0900

    멀티탭 타임에러

--- multitap.py
+++ multitap.py
@@ -1,24 +1,30 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+N, K = map(int, input().split())
 
 array = list(map(int,input().split()))
 
-# 멀티텝을 써야하는 횟수를 우선순위로 설정
-use = []
-for i in array:
-    use.append([array.count(i),i])
+plug_count = 0
+multi_tap = array[:N]
+for i in range(N,K):
+    # 멀티탭 안에 이미 꽂혀있다면
+    if array[i] in multi_tap:
+        continue
+    # 중복된 코드가 없다면 
+    # 코드들의 인덱스를 넣어주는 리스트 선언
+    code_idx = []
+    # 멀티탭의 코드들을 확인
+    for code in multi_tap:
+        try:
+            idx = array[i:].index(code)
+        except:
+            idx = 101
+        code_idx.append(idx)
+    # 빼야하는 코드
+    to_out  = code_idx.index(max(code_idx))
+    del multi_tap[to_out]
+    multi_tap.append(array[i])
+    plug_count += 1
 
-result = 0
-multi_tap = use[0:n]
-# prior은 우선순위(사용빈도), thing은 전기용품 종류
-for prior, thing in use[n:k]:
-    if [prior,thing] in multi_tap:
-        heapq.heappush(multi_tap, [prior-1, thing])
-    else:
-        heapq.heappop(multi_tap)
-        heapq.heappush(multi_tap, [prior, thing])
-        result += 1
-print(result)
+print(plug_count)
