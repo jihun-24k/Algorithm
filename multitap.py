eaeaeb4da565bf71db7a9c71236ac6ca9643ad82
jihun <gi020477@gmail.com>
2021-02-15 23:28:15 +0900

    멀티탭 런타임에러 2일차..

--- multitap.py
+++ multitap.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
@@ -6,25 +7,30 @@ N, K = map(int, input().split())
 array = list(map(int,input().split()))
 
 plug_count = 0
-multi_tap = array[:N]
-for i in range(N,K):
-    # 멀티탭 안에 이미 꽂혀있다면
-    if array[i] in multi_tap:
+multi_tap = []
+for i in range(K):
+    # 현재 코드의 다음 코드의 순서를 계산
+    # 만일 없다면 100보다 큰 101값을 넣어줌
+    try:
+        next_idx = array[i+1:].index(array[i])
+    except:
+        next_idx = 101
+    
+    # 멀티탭이 다 안찼다면
+    if len(multi_tap) < N:
+        heapq.heappush(multi_tap,(-next_idx, array[i]))
         continue
-    # 중복된 코드가 없다면 
-    # 코드들의 인덱스를 넣어주는 리스트 선언
-    code_idx = []
-    # 멀티탭의 코드들을 확인
-    for code in multi_tap:
-        try:
-            idx = array[i:].index(code)
-        except:
-            idx = 101
-        code_idx.append(idx)
-    # 빼야하는 코드
-    to_out  = code_idx.index(max(code_idx))
-    del multi_tap[to_out]
-    multi_tap.append(array[i])
+    # 멀티탭 안에 이미 꽂혀있다면
+    # 같은 코드가 있다면
+    if array[i] == multi_tap[-1][1]:
+        del multi_tap[-1]
+        heapq.heappush(multi_tap,(-next_idx, array[i]))
+        continue
+    if array[i] == multi_tap[-2][1]:
+        del multi_tap[-2]
+        heapq.heappush(multi_tap,(-next_idx, array[i]))
+        continue
+    heapq.heappop(multi_tap)
+    heapq.heappush(multi_tap,(-next_idx, array[i]))
     plug_count += 1
-
 print(plug_count)
