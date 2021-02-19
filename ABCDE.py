a7233f4553299e9c7b05fbcda05db4022f352683
jihun <gi020477@gmail.com>
2021-02-19 15:36:28 +0900

    ABCDE

--- ABCDE.py
+++ ABCDE.py
@@ -1,3 +1,5 @@
+# 백준 13023번 ABCDE
+
 n, m = map(int,input().split())
 
 friends = [[]*i for i in range(n)]
@@ -9,22 +11,25 @@ for _ in range(m):
 
 # 방문됐는지 
 visited = [False]*n
-depth = [0] * n
+result = 0
 
-def dfs (graph, v, visited, depth):
+def dfs (v, depth):
     visited[v] = True
-    depth[v] += 1
-
-    for i in graph[v]:
-        if not visited[i]:
-            depth[i] = depth[v]
-            dfs(graph,i,visited,depth)
+    global result
+    # ABCDE의 친구 관계가 있다면
+    if depth >= 4:
+        result = 1
+    else:
+        # v 시작점부터 깊이를 탐색
+        for i in friends[v]:
+            if not visited[i]:
+                dfs(i,depth+1)
+        # 만약 마지막 정점이라면 False로 초기화
+        visited[v] = False
 
 for i in range(n):
-    dfs(friends, i, visited, depth)
-    if max(depth) >= 5:
-        print(1)
+    dfs(i, 0)
+    if result:
         break
 
-if max(depth) < 5:
-    print(0)
+print(result)
