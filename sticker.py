8ad0e740e192b34a39a585a2390553985782c638
jihun <gi020477@gmail.com>
2021-03-01 22:41:08 +0900

    스티커

--- sticker.py
+++ sticker.py
@@ -8,42 +8,21 @@ for _ in range(T):
     case = []
     N = int(input())
     for _ in range(2):
-        case.append(list(map(int,input().split())))
+        # 1부터 스티커의 인덱스 시작
+        case.append([0]+list(map(int,input().split())))
     test_case.append(case)
 
+# 한 케이스씩 검사
 for case in test_case:
-    # 스티커 판별
+    # 스티커만큼의 이차원 dp를 초기화
     width = len(case[0])
-    result = 0
-    # 이전 인덱스 값
-    before = -1
+    dp = [[0 for _ in range(width)] for _ in range(2)]
 
-    for i in range(1, width):
-        route1 = case[0][i-1] + case[1][i]
-        route2 = case[1][i-1] + case[0][i]
-        # 이전 인덱스랑 같은 스티커는 빼준다
-        if before == 0:
-            route1 -= case[0][i-1]
-        elif before == 1:
-            route2 -= case[1][i-1]
+    dp[0][1] = case[0][1]
+    dp[1][1] = case[1][1]
 
-        # 만약 최대값이 같을 때
-        if route1 == route2:
-            result += route1
-            before = -1
-            continue
-        # 두 루트가 차이가 날 때
-        if route1 > route2:
-            if i == width -1:
-                result += route1
-                continue
-            result += case[0][i-1]
-            before = 0
-        else:
-            if i == width -1:
-                result += route2
-                continue   
-            result += case[1][i-1]
-            before = 1
-        print(result)
-    print("****")
+    # 현재 스티커를 선택했다고 생각하고 계산한다.
+    for n in range(2, width):
+        dp[0][n] = max(dp[1][n-2], dp[1][n-1]) + case[0][n]
+        dp[1][n] = max(dp[0][n-2], dp[0][n-1]) + case[1][n]
+    print(max(dp[0][-1], dp[1][-1]))
