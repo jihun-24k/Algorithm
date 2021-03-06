5a28a1f016b77f541e7dc2e403584c04afc79bd0
bluesweetpotato <gi020477@gmail.com>
2021-03-06 20:53:02 +0900

    Z

--- Z.py
+++ Z.py
@@ -4,22 +4,22 @@ input = sys.stdin.readline
 
 N, r, c = map(int,input().split())
 
-# 시간 제약이 많고 메모리는 제약은 작다.
-matrix = [[0 for _ in range(2**N+1)] for _ in range(2**N+1)]
-
-print(matrix)
-
-def z_route(x,y,n):
-    global N
-    # 작은 사각형일 때
-    if N == 1:
-        matrix[x][y+1] = matrix[x][y] +1    
-        matrix[x+1][y] = matrix[x][y+1] +1        
-        matrix[x+1][y+1] = matrix[x][y] +1        
-        matrix[x][y+1] = matrix[x][y] +1       
-
+def z_route(x,y,n,start):
+    # 해당 인덱스를 찾았으면 그 값을 반환한다.
+    if x == r and y == c:
+        print(start)
     else:
-        z_route(x//2,y//2,N//2)
-        z_route(x//2,y//2,N//2)
-        z_route(x//2,y//2,N//2)
-        z_route(x//2,y//2,N//2)
+        # 1 사면체
+        if (r < x+ 2**(n-1) and c < y + 2**(n-1)):
+            z_route(x, y, n-1, start)
+        # 2 사면체
+        if (r < x+ 2**(n-1) and c >= y + 2**(n-1)):
+            z_route(x ,y + 2**(n-1) ,n-1, start + 2**(2*(n-1)))
+        # 3 사면체
+        if (r >= x+ 2**(n-1) and c < y + 2**(n-1)):
+            z_route(x+ 2**(n-1) ,y, n-1 , start + 2*2**(2*(n-1)))
+        # 4 사면체
+        if (r >= x+ 2**(n-1) and c >= y + 2**(n-1)):
+            z_route(x+ 2**(n-1) ,y+ 2**(n-1), n-1 ,start + 3*2**(2*(n-1)))
+
+z_route(0,0,N,0)
