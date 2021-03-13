f5f43f790eaef59ed6ec9fd9e06b8a80635f143e
bluesweetpotato <gi020477@gmail.com>
2021-03-13 21:42:59 +0900

    출력초과 및 런타임에러

--- tree_traversal2.py
+++ tree_traversal2.py
@@ -7,44 +7,27 @@ n = int(input())
 in_order = list(map(int, input().split()))
 post_order = list(map(int, input().split()))
 
-# 각각의 방문 리스트를 생성
+# 방문 확인
 in_visited = [False]*len(in_order)
 post_visited = [False]*len(post_order)
 
-# 트리 생성
-class Node:
-    def __init__(self, data, left_node, right_node):
-        self.data = data
-        self.left_node = left_node
-        self.right_node = right_node
+# 후위 순회의 제일 마지막 노드는 루트 노드다
+queue = deque()
+queue.append(post_order[-1])
 
-# 트리 입력
-tree = {}
+while queue:
+    root = queue.popleft()
 
-for i in range(n):
+    i_idx = in_order.index(root)
+    p_idx = post_order.index(root)
 
-    # 후위순회의 제일 마지막 노드가 루트 노드이다.
-    queue = deque(post_order[-1])
+    in_visited[i_idx] = True
+    post_visited[p_idx] = True
+    print(root, end=' ')
 
-    while queue:
-        v = queue.popleft()
-        # 루트의 중위,후위 인덱스
-        i_idx = in_order.index(v)
-        p_idx = post_order.index(v)
-
-        in_visited[i_idx] = True
-        post_visited[p_idx] = True
-
-        if i_idx ==0 or p_idx ==0:
-            
-
-    data, left_node, right_node = map(int,input().split())
-    tree[data] = Node(data, left_node, right_node)
-
-# 전위 순회 (Preorder Traversal)
-def pre_order (node):
-    print(node.data, end='')
-    if node.left_node != '.':
-        pre_order(tree[node.left_node])
-    if node.right_node != '.':
-        pre_order(tree[node.right_node])
+    # 왼쪽 서브 트리
+    if i_idx != 0 and in_visited[i_idx-1] != True:
+        queue.append(in_order[i_idx-1])
+    # 오른쪽 서브 트리
+    if p_idx !=0 and post_visited[p_idx-1] != True:
+        queue.append(post_order[p_idx-1])
