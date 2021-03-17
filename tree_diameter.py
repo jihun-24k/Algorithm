55e14f77ea125340bed02a4d225b683bd3041dd3
jihun <gi020477@gmail.com>
2021-03-18 02:59:28 +0900

    트리순회 1967

--- tree_diameter.py
+++ tree_diameter.py
@@ -1,6 +1,39 @@
 # 백준 1967번 트리의 지름 (골4)
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def dfs(node, root):
-    if visited
+N = int(input())
+
+tree = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    n1, n2, weight = map(int,input().split())
+    tree[n1].append([n2, weight])
+    tree[n2].append([n1, weight])
+
+
+def bfs(root):
+    queue = deque()
+    queue.append([root, 0])
+    visited = [False] * (N+1)
+    
+    max_value = 0
+    max_node = 0
+
+    while queue:
+        node, weight = queue.popleft()
+        visited[node] = True
+
+        for n, w in tree[node]:
+
+            if not visited[n]:
+                visited[n] = True
+                queue.append([n, w + weight])
+
+                if w + weight > max_value:
+                    max_node = n
+                    max_value = w + weight
+
+    return max_node, max_value
+
+print(bfs(bfs(1)[0])[1])
