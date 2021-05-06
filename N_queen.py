14b3cfc38d8d918cf45992a49c5fd0008acb3204
jihun kim <jihunkim@jihunui-MacBookAir.local>
2021-05-06 23:44:25 +0900

    N_queen

--- N_queen.py
+++ N_queen.py
@@ -1,24 +1,25 @@
-# 백준 9663번 N-queen
+# 백준 9663번 N-Queen
 
 N = int(input())
 
-col = [0] * (N + 1)
+queen = [-1] * N
 result = 0
 
-def check_queen(row, col):
-    for i in range(1, row):
-        if (abs(col[i] - col[row]) == row - i or col[i] == col[row]):
-            return False
-    return True
+def check_queen(col, row):
+    for i in range(row):
+        # 대각선
+        if abs(queen[i] - queen[row]) == row - i or queen[i] == queen[row]:
+            return True
+    return False
 
-def dfs(row, col):
+def dfs(row):
     global result
-    if check_queen(row, col):
-        if row == N:
-            result += 1
-        else:
-            for k in range(1, N + 1):
-                col[row + 1] = k
-                dfs(row + 1, col)
-dfs(0, col)
+    if row == N:
+        result += 1
+    else:
+        for col in range(N):
+            queen[row] = col
+            if not (check_queen(col, row)):
+                dfs(row + 1)
+dfs(0)
 print(result)
