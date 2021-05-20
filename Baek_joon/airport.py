dbe165348fe29cc18aa8abe9fef54c242e799f23
jihun-24k <gi020477@gmail.com>
2021-05-21 00:52:25 +0900

    공항

---
+++ Baek_joon/airport.py
@@ -0,0 +1,38 @@
+# 백준 10775번 공항
+
+# union find 이론
+# 게이트 찾기
+def find_gate(gates, plane):
+    if gates[plane] != plane:
+        gates[plane] = find_gate(gates, gates[plane])
+    return gates[plane]
+# 합치기
+def union_gate(gates, a, b):
+    a = find_gate(gates, a)
+    b = find_gate(gates, b)
+
+    gates[a] = b
+
+G = int(input())
+P = int(input())
+
+result = 0
+# 게이트 초기화
+gates = [0] * (G + 1)
+for i in range(1, G + 1):
+    gates[i] = i
+
+# 비행기 입력
+plane = [0] * (P + 1)
+for i in range(1, P + 1):
+    plane[i] = int(input())
+
+# 연산 시작
+for i in range(1, P + 1):
+    docking = find_gate(gates, plane[i])
+    if docking != 0:
+        union_gate(gates, docking, docking - 1)
+        result += 1
+    else:
+        break
+print(result)
