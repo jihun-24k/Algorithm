922a2940031a76ec7ff5224f754c7f0c54d44847
jihun-24k <gi020477@gmail.com>
2021-05-23 16:49:29 +0900

    nq

--- Baek_joon/N_queen.py
+++ Baek_joon/N_queen.py
@@ -1,26 +1,25 @@
 # 백준 9663번 N-Queen
 
-N = int(input())
-
-visited = [False] * N
-result = [0]
-
 def check_queen(queen, col):
     n = len(queen)
     for i in range(n):
         # 대각선
         if abs(queen[i] - col) == abs(n - i):
-            return True
-    return False
+            return False
+    return True
 
 def dfs(queen, row):
     if row == N:
         result[0] += 1
     else:
         for col in range(N):
-            if not (check_queen(queen, col)) and not visited[col]:
+            if check_queen(queen, col) and not visited[col]:
                 visited[col] = True
                 dfs(queen + [col] ,row + 1)
                 visited[col] = False
+
+N = int(input())
+visited = [False] * N
+result = [0]
 dfs([], 0)
 print(*result)
