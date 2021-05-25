6175b11bedb76f6599f438b8246893d47a220097
jihun-24k <gi020477@gmail.com>
2021-05-25 23:48:35 +0900

    텀프로젝트 메모리 초과

--- Baek_joon/term_project.py
+++ Baek_joon/term_project.py
@@ -1,5 +1,6 @@
 # 백준 9466번 텀 프로젝트
 import sys
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 T = int(input())
@@ -8,27 +9,27 @@ for _ in range(T):
     t = int(input())
     test_case.append([0]+list(map(int,input().split())))
 
-def find_team(case ,visited ,start ,root , ans, result):
-    visited[root] = True
+def find_team(x, result):
+    visited[x] = True
+    cycle.append(x)
+    num = case[x]
+
     # 종료 조건
-    if start == case[root]:
-        result += ans
-    elif case[root] == root:
-        result += 0
+    if visited[num]:
+        if num in cycle:
+            result += cycle[cycle.index(num):]
+        return
     else:
-        find_team(case ,visited ,start ,case[root] ,ans + 1, result)
-        visited[root] = False
+        find_team(num, result)
 
 # 테스트 케이스마다 반복
 for case in test_case:
-    visited = [False for _ in range(len(case))]
-    result = 0
+    visited = [True] + [False for _ in range(len(case)-1)]
+    result = []
 
     # 케이스들의 노드들을 확인
     for idx in range(1,len(case)):
-        # 이미 방문한 노드라면 넘어간다
-        if visited[idx]:
-            continue
-        find_team(case ,visited ,idx ,idx ,1, result)
-
-    print(result)
+        if not visited[idx]:
+            cycle = []
+            find_team(idx, result)
+    print(len(case) - len(result) - 1)
