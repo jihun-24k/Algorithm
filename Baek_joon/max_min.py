7bced00c5c61fbf470c9f10562c00ea7cbeaa615
jihun-24k <gi020477@gmail.com>
2021-06-16 23:56:39 +0900

    최솟값 최댓값

--- Baek_joon/max_min.py
+++ Baek_joon/max_min.py
@@ -4,47 +4,65 @@ sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-nums = [0] + [int(input()) for _ in range(N)]
+nums = [int(input()) for _ in range(N)]
 a_b = [list(map(int, input().split())) for _ in range(M)]
 
 # 세그먼트 트리
 # tree1 = 최소값을 담은 트리
 # tree2 = 최댓값을 담은 트리
-tree1 = [0] * 3000000
-tree2 = [0] * 3000000
+tree1 = [0] * 30
+tree2 = [0] * 30
 
-def init_min(node, start, end):
+def init(node, start, end):
     if start == end:
         tree1[node] = nums[start]
-    else:
-        tree1[node] = init_min(node*2, start, (start+end)//2) + init_min(node*2+1, (start+end)//2+1, end)
-        return tree1[node]
-
-def init_max(node, start, end):
-    if start == end:
         tree2[node] = nums[start]
     else:
-        tree2[node] = max(init_min(node*2, start, (start+end)//2), init_min(node*2+1, (start+end)//2+1, end))
-        return tree2[node]
+        init(node*2, start, (start+end)//2)
+        init(node*2+1, (start+end)//2+1, end)
+        if nums[start] < nums[end]:
+            tree1[node] = nums[start]
+            tree2[node] = nums[end]
+        else:
+            tree1[node] = nums[end]
+            tree2[node] = nums[start]
 
 def find_min(node, start, end, left, right):
     if left > end or right < start:
-        return 0
+        return -1
     if left <= start and end <= right:
         return tree1[node]
-    return min(find_min(node * 2, start, (start + end)//2, left, right), find_min(node*2+1, (start+end)//2+1, end))
+    m1 = find_min(node * 2, start, (start + end)//2, left, right)
+    m2 = find_min(node * 2 + 1, (start+end)//2+1, end, left, right)
+    
+    if m1 == -1:
+        return m2
+    elif m2 == -1:
+        return m1
+    else:
+        return min(m1, m2)
 
 def find_max(node, start, end, left, right):
     if left > end or right < start:
-        return 0
+        return -1
     if left <= start and end <= right:
         return tree2[node]
-    return max(find_max(node * 2, start, (start + end)//2, left, right), find_max(node*2+1, (start+end)//2+1, end))
+    m1 = find_max(node * 2, start, (start + end)//2, left, right)
+    m2 = find_max(node * 2 + 1, (start+end)//2 + 1, end, left, right)
 
-init_min(1, 0, N-1)
-init_max(1, 0, N-1)
+    if m1 == -1:
+        return m2
+    elif m2 == -1:
+        return m1
+    else:
+        return max(m1, m2)
+
+init(1, 0, N-1)
+print(nums)
+print(tree1)
+print(tree2)
 
 for a, b in a_b:
-    min_v = find_min(1, 0, N-1, a - 1, b - 1)
-    max_v = find_max(1, 0, N-1, a - 1, b - 1)
+    min_v = find_min(1, 0, N - 1, a - 1, b - 1)
+    max_v = find_max(1, 0, N - 1, a - 1, b - 1)
     print(min_v, max_v)
