07a687208dafd96a16b3bd2e72b8a5e2f03f07b4
jihun-24k <gi020477@gmail.com>
2021-06-17 01:12:49 +0900

    최소값 최댓값 세그먼트 트리

--- Baek_joon/max_min.py
+++ Baek_joon/max_min.py
@@ -10,22 +10,24 @@ a_b = [list(map(int, input().split())) for _ in range(M)]
 # 세그먼트 트리
 # tree1 = 최소값을 담은 트리
 # tree2 = 최댓값을 담은 트리
-tree1 = [0] * 30
-tree2 = [0] * 30
+tree1 = [0] * 3000000
+tree2 = [0] * 3000000
 
-def init(node, start, end):
+def init_min(node, start, end):
     if start == end:
         tree1[node] = nums[start]
-        tree2[node] = nums[start]
+        return tree1[node]
     else:
-        init(node*2, start, (start+end)//2)
-        init(node*2+1, (start+end)//2+1, end)
-        if nums[start] < nums[end]:
-            tree1[node] = nums[start]
-            tree2[node] = nums[end]
-        else:
-            tree1[node] = nums[end]
-            tree2[node] = nums[start]
+        tree1[node] = min(init_min(node*2, start, (start+end)//2), init_min(node*2+1, (start+end)//2+1, end))
+        return tree1[node]
+
+def init_max(node, start, end):
+    if start == end:
+        tree2[node] = nums[start]
+        return tree2[node]
+    else:
+        tree2[node] = max(init_max(node*2, start, (start+end)//2), init_max(node*2+1, (start+end)//2+1, end))
+        return tree2[node]
 
 def find_min(node, start, end, left, right):
     if left > end or right < start:
@@ -47,20 +49,18 @@ def find_max(node, start, end, left, right):
         return -1
     if left <= start and end <= right:
         return tree2[node]
-    m1 = find_max(node * 2, start, (start + end)//2, left, right)
-    m2 = find_max(node * 2 + 1, (start+end)//2 + 1, end, left, right)
+    n1 = find_max(node * 2, start, (start + end)//2, left, right)
+    n2 = find_max(node * 2 + 1, (start+end)//2 + 1, end, left, right)
 
-    if m1 == -1:
-        return m2
-    elif m2 == -1:
-        return m1
+    if n1 == -1:
+        return n2
+    elif n2 == -1:
+        return n1
     else:
-        return max(m1, m2)
+        return max(n1, n2)
 
-init(1, 0, N-1)
-print(nums)
-print(tree1)
-print(tree2)
+init_min(1, 0, N-1)
+init_max(1, 0, N-1)
 
 for a, b in a_b:
     min_v = find_min(1, 0, N - 1, a - 1, b - 1)
