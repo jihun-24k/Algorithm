c8db3bdb7cc56ed431ff4e398135c16da5aadc0b
jihun-24k <gi020477@gmail.com>
2022-02-01 17:56:20 +0900

    failed: 뱀과 사다리 게임

---
+++ Baek_joon/ladder_snake.py
@@ -0,0 +1,23 @@
+# 백준 16928번 뱀과 사다리 (실버 1)
+from collections import deque
+
+dp = [-1 for _ in range(101)]
+graph = [*range(101)]
+queue = deque([1])
+while queue:
+    now = queue.popleft()
+    for i in range(1,7):
+        next = now + i
+        if next > 100:
+            break
+        next = graph[next]
+        if(dp[next] == -1 or dp[next] > dp[now] + 1):
+            dp[next] = dp[now] + 1
+            queue.append(next)
+
+N, M = map(int, input().split())
+for _ in range(N + M):
+    x, y = map(int, input().split())
+    graph[x] = y
+
+print(dp[-1])
