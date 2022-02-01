03b4c599fcd2483e21bdd671e272c39c9864b473
jihun-24k <gi020477@gmail.com>
2022-02-02 00:11:07 +0900

    solved: 뱀과 사다리 게임

--- Baek_joon/ladder_snake.py
+++ Baek_joon/ladder_snake.py
@@ -1,9 +1,16 @@
 # 백준 16928번 뱀과 사다리 (실버 1)
 from collections import deque
 
-dp = [-1 for _ in range(101)]
+dp = [-1] * 101
+dp[1] = 0
 graph = [*range(101)]
 queue = deque([1])
+
+N, M = map(int, input().split())
+for _ in range(N + M):
+    x, y = map(int, input().split())
+    graph[x] = y
+
 while queue:
     now = queue.popleft()
     for i in range(1,7):
@@ -14,10 +21,5 @@ while queue:
         if(dp[next] == -1 or dp[next] > dp[now] + 1):
             dp[next] = dp[now] + 1
             queue.append(next)
-
-N, M = map(int, input().split())
-for _ in range(N + M):
-    x, y = map(int, input().split())
-    graph[x] = y
 
 print(dp[-1])
