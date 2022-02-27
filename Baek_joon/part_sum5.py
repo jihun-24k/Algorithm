f9a8a59250718992ad26de8091d67a3d7bf59345
jihun-24k <gi020477@gmail.com>
2022-02-27 11:05:09 +0900

    failed: 구간 합 구하기5 (시간초과)

---
+++ Baek_joon/part_sum5.py
@@ -0,0 +1,17 @@
+# 백준 11660번 구간 합 구하기5 (실버1)
+N, M = map(int, input().split())
+
+square = []
+dp = [[0] * (N + 1) for _ in range(N + 1)]
+
+for _ in range(N):
+    row = list(map(int, input().split()))
+    square.append(row)
+
+for i in range(1, N+1):
+    for j in range(1, N + 1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + square[i-1][j-1]
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    print(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1])
