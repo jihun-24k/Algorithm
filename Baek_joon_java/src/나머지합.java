c881888e9776b397d0d23072e438bb762800039c
jihun-24k <gi020477@gmail.com>
2022-12-14 22:13:44 +0900

    Solve: 나머지합

--- Baek_joon_java/src/나머지합.java
+++ Baek_joon_java/src/나머지합.java
@@ -1,28 +1,34 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class 나머지합 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] A = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            int number = Integer.parseInt(stringTokenizer.nextToken());
-            A[i] = A[i - 1] + number;
+        long[] A = new long[N];
+        long[] counts = new long[M];
+        long result = 0;
+        A[0] = sc.nextInt();
+
+        for (int i = 1; i < N; i++) {
+            A[i] = A[i - 1] + sc.nextInt();
         }
 
-        int result = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = i; j <= N; j++) {
-                if ((A[j] - A[i-1]) % M == 0) result += 1;
+        for (int i = 0; i < N; i++) {
+            int remainder = (int) A[i] % M;
+            if (remainder == 0) {
+                result++;
+            }
+            counts[remainder]++;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (counts[i] > 1) {
+                result += (counts[i] * (counts[i] - 1) / 2);
             }
         }
         System.out.println(result);
