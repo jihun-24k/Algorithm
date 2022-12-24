b363a6992ea83ca8c9f438f0adb04c68fe089d7d
jihun-24k <gi020477@gmail.com>
2022-12-24 23:01:17 +0900

    Fail: Collection 도입

--- Baek_joon_java/src/DNA비밀번호.java
+++ Baek_joon_java/src/DNA비밀번호.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class DNA비밀번호 {
@@ -13,10 +17,31 @@ public class DNA비밀번호 {
         int P = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(bf.readLine());
-        String DNA = st.nextToken();
+        List<String> DNA = Arrays.asList(st.nextToken());
 
         st = new StringTokenizer(bf.readLine());
-        String[] ch = {"A", "C", "G", "T"};
+        List<String> charList = Arrays.asList("A", "C", "G", "T");
+        Map<String, Integer> count = new HashMap<>();
+        for (String ch : charList) {
+            count.put(ch, Integer.valueOf(st.nextToken()));
+        }
+
+        for (int i = 0; i < P; ) {
+            for (int i = P; i <= S; i++) {
+                add();
+                delete();
+            }
+        }
 
     }
+
+    public static void add() {
+
+    }
+
+    public static void delete() {
+
+    }
+
+
 }
