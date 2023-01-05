df7a86fab99a5de23ff8b4cc83fcb5bef895ed45
jihun-24k <gi020477@gmail.com>
2023-01-05 14:01:38 +0900

    Solve: 스택수열

--- Baek_joon_java/src/자료구조/스택수열.java
+++ Baek_joon_java/src/자료구조/스택수열.java
@@ -1,6 +1,9 @@
 package 자료구조;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class 스택수열 {
 
@@ -8,8 +11,32 @@ public class 스택수열 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
+        int number = 1;
+        boolean isNo = false;
+        Stack<Integer> stack = new Stack<>();
+        List<String> operations = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            
+            int checkNum = sc.nextInt();
+            if (number <= checkNum) {
+                while (number <= checkNum) {
+                    stack.push(number++);
+                    operations.add("+");
+                }
+                stack.pop();
+                operations.add("-");
+            } else {
+                int popNum = stack.pop();
+                if (popNum > checkNum) {
+                    System.out.println("NO");
+                    isNo = true;
+                    break;
+                } else {
+                    operations.add("-");
+                }
+            }
+        }
+        if (!isNo) {
+            operations.forEach(System.out::println);
         }
     }
 }
