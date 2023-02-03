639a9c5f03a309fb8821868523aede28a6754b97
jihun-24k <gi020477@gmail.com>
2023-02-03 23:21:33 +0900

    Solve: 소트인사이드

---
+++ Baek_joon_java/src/자료구조/소트인사이드.java
@@ -0,0 +1,24 @@
+package 자료구조;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 소트인사이드 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] number = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            number[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+
+        Arrays.sort(number);
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(number[i]);
+        }
+    }
+
+}
