063b8ab42c82572752bdf42222dc99b8e7ab5c68
jihun-24k <gi020477@gmail.com>
2023-02-14 12:17:18 +0900

    Solve: DFS와 BFS

---
+++ Baek_joon_java/src/자료구조/DFS와BFS.java
@@ -0,0 +1,77 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class DFS와BFS {
+
+    static ArrayList<Integer>[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr[s].add(e);
+            arr[e].add(s);
+        }
+
+        dfs(V);
+        System.out.println();
+        visited = new boolean[N + 1];
+        bfs(V);
+    }
+
+    private static void dfs(int v) {
+        if (visited[v]) {
+            return;
+        }
+        Collections.sort(arr[v]);
+        visited[v] = true;
+        System.out.print(v + " ");
+        for (int num : arr[v]) {
+            if (!visited[num]) {
+                dfs(num);
+            }
+        }
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+        visited[v] = true;
+        System.out.print(v + " ");
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int n : arr[node]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.offer(n);
+                    System.out.print(n + " ");
+                }
+            }
+
+        }
+    }
+}
