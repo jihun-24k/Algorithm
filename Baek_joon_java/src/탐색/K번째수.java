c04d2477b91fe1deeb0d71c01259e0751993e155
jihun-24k <gi020477@gmail.com>
2023-02-26 21:27:12 +0900

    Solve: K번째 수

--- Baek_joon_java/src/탐색/K번째수.java
+++ Baek_joon_java/src/탐색/K번째수.java
@@ -1,27 +1,30 @@
 package 탐색;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class K번째수 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int k = sc.nextInt();
 
-        int[] B = new int[k + 1];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (N * i + j - 1 >= k) {
-                    break;
-                }
-                B[N * i + j - 1] = (i + 1) * j;
+        long start = 1;
+        long end = k;
+        long ans = 0;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            long cnt = 0;
+            for (long i = 1; i <= N; i++) {
+                cnt += Math.min(N, mid / i);
+            }
+            if (cnt < k) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                end = mid - 1;
             }
         }
-
-        Arrays.sort(B);
-        System.out.println(B[k]);
+        System.out.println(ans);
     }
 }
