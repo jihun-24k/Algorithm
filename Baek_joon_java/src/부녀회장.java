56d8d070a36dd83b22622c50a4672d674fae32cc
jihun-24k <gi020477@gmail.com>
2023-03-13 09:49:17 +0900

    Solve: 부녀회장

---
+++ Baek_joon_java/src/부녀회장.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class 부녀회장 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int[][] apt = new int[15][15];
+        for (int i = 1; i < 15; i++) {
+            apt[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 1; j < 15; j++) {
+                apt[i][j] = apt[i - 1][j] + apt[i][j - 1];
+            }
+        }
+
+        for (int t = 0; t < T; t++) {
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+
+            System.out.println(apt[k][n]);
+        }
+    }
+}
