0f6ced67b444e28b2f75311bcd10e2b0218d53ce
jihun-24k <gi020477@gmail.com>
2023-03-20 17:15:38 +0900

    Solve: 쉬운 계단수

---
+++ Baek_joon_java/src/쉬운계단수.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class 쉬운계단수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long mod = 1000000000;
+
+        long[][] dp = new long[101][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][0] = dp[i - 1][1];
+                } else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8];
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1] % mod;
+                }
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i < 10; i++) {
+            result += dp[N][i];
+        }
+        System.out.println(result % mod);
+    }
+}
