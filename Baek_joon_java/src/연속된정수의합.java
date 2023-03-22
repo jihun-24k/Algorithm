beee8170c508ac2cfb668cf3210cf8e42a9ae87c
jihun-24k <gi020477@gmail.com>
2023-03-22 11:03:48 +0900

    Solve: 연속된수열합

---
+++ Baek_joon_java/src/연속된정수의합.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연속된정수의합 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] left = new int[n];
+        int result = arr[0];
+        left[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(arr[i], left[i - 1] + arr[i]);
+            result = Math.max(left[i], result);
+        }
+
+        int[] right = new int[n];
+        right[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = Math.max(arr[i], right[i + 1] + arr[i]);
+        }
+
+        for (int i = 1; i < n - 1; i++) {
+            result = Math.max(result, left[i - 1] + right[i + 1]);
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
