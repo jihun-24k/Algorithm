1ebd7f4e172aab132c93ad7455fcf390dc8cf5af
jihun-24k <gi020477@gmail.com>
2023-03-24 09:46:17 +0900

    Solve: CCW

---
+++ Baek_joon_java/src/선분방향구하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class 선분방향구하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+
+        int result = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3);
+        if (result < 0) {
+            System.out.println(-1);
+        } else if (result == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
+    }
+}
